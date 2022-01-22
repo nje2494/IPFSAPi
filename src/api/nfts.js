a1cd4cadafa71b935ce765d50241a4e1f726b7a4
Nadim El-Jaroudi <nje2494@gmail.com>
2022-01-22 17:57:09 -0500

    Added error handling for incorrect address

--- src/api/nfts.js
+++ src/api/nfts.js
@@ -10,13 +10,16 @@ const router = express.Router();
 const { ethers } = require("ethers");
 const axios = require("axios");
 
-const pinataSDK = require('@pinata/sdk');
+const pinataSDK = require("@pinata/sdk");
 
-const pinata = pinataSDK('c17edaf981e8b9b8e3a9', 'ab8c5a58dab5d97a888d8d1f922db6772e7b016ec6aa0217e0b56599c3597a36');
+const pinata = pinataSDK(
+  "c17edaf981e8b9b8e3a9",
+  "ab8c5a58dab5d97a888d8d1f922db6772e7b016ec6aa0217e0b56599c3597a36"
+);
 
 const WEB3_ENDPOINT = "https://cloudflare-eth.com";
 
-const handleError = () => undefined;
+const handleError = () => "failed";
 
 const getTokenMetadata = async (address, tokenID) => {
   const abi = ["function tokenURI(uint256 _tokenId) view returns (string URI)"];
@@ -40,17 +43,21 @@ router.get("/:contract/:token/:chain", (req, res) => {
   var chain = req.params.chain;
   var jsonBuilder = {};
   getTokenMetadata(contract, tokenID).then((token) => {
-    console.log(token[0]);
-    let uri = token[0].toString();
-    if (uri.substring(0, 4) === "ipfs") {
-      uri = `https://gateway.pinata.cloud/${uri.substring(6)}`;
-    }
-    axios.get(uri).then((response) => {
-      console.log(response.data);
-      pinata.pinJSONToIPFS(response.data, null).then((response2) => {
-        res.json(response2);
+    if (token[0] === "failed") {
+      res.json({ IpfsHash: "not found" });
+    } else {
+      console.log(token[0]);
+      let uri = token[0].toString();
+      if (uri.substring(0, 4) === "ipfs") {
+        uri = `https://gateway.pinata.cloud/${uri.substring(6)}`;
+      }
+      axios.get(uri).then((response) => {
+        console.log(response.data);
+        pinata.pinJSONToIPFS(response.data, null).then((response2) => {
+          res.json(response2);
+        });
       });
-    });
+    }
   });
 });
 
